86b2976905821bb0f4366876b451167db29083f8
hayanbeigh <hayanbeigh117@gmail.com>
2024-05-28 16:30:49 +0530

    add: auth interceptor

--- lib/infrastructure/core/dio_module.dart
+++ lib/infrastructure/core/dio_module.dart
@@ -1,13 +1,21 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:vtrack_v1/globals.dart';
+import 'package:vtrack_v1/infrastructure/core/auth_interceptor.dart';
 
 @module
 abstract class DioModule {
   @lazySingleton
-  Dio get dio => Dio(
-        BaseOptions(
-          baseUrl: Globals.apiUrl!,
-        ),
-      );
+  Dio get dio {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: Globals.apiUrl!,
+      ),
+    );
+
+    // Add the interceptor
+    dio.interceptors.add(AuthInterceptor());
+
+    return dio;
+  }
 }
