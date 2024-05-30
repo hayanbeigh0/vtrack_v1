7bf08a03cd9efe26f4498fb9bc3ac3c5d3284e97
hayanbeigh <hayanbeigh117@gmail.com>
2024-05-30 15:58:10 +0530

    change: folder structure in application layer

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -13,13 +13,14 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:shared_preferences/shared_preferences.dart' as _i4;
 import 'package:vtrack_v1/application/auth/auth_bloc/auth_bloc.dart' as _i14;
-import 'package:vtrack_v1/application/auth/sign_in_form/bloc/sign_in_form_bloc.dart'
+import 'package:vtrack_v1/application/auth/sign_in_form_bloc/sign_in_form_bloc.dart'
     as _i15;
-import 'package:vtrack_v1/application/current_user/current_user_cubit.dart'
+import 'package:vtrack_v1/application/current_user/current_user_cubit/current_user_cubit.dart'
     as _i19;
-import 'package:vtrack_v1/application/organisation_cubit/organisation_cubit.dart'
+import 'package:vtrack_v1/application/organisation/organisation_cubit/organisation_cubit.dart'
     as _i17;
-import 'package:vtrack_v1/application/vehicle/vehicle_cubit.dart' as _i18;
+import 'package:vtrack_v1/application/vehicle/vehicle_cubit/vehicle_cubit.dart'
+    as _i18;
 import 'package:vtrack_v1/domain/auth/i_auth_facade.dart' as _i10;
 import 'package:vtrack_v1/domain/organisation/i_organisation.dart' as _i6;
 import 'package:vtrack_v1/domain/user/i_user.dart' as _i12;
